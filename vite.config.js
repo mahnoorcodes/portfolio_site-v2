cc3b042187f696d50c990277c7587064c9308595
mahnoorcodes <mahnoor.faisal.h@gmail.com>
2026-09-22 23:20:13 +0400

    change base path name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/portfolio_site-v2",
+  base: process.env.VITE_BASE_PATH || "/freelance",
   css:{
     modules:{
       localsConvention: "camelCase",
